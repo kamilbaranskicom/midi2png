c83ca2dd892354e7d836b81571a613ba6065d91d
Kamil Barański <kamilbaranski@gmail.com>
2021-03-18 03:43:31 +0100

    written extractNoteOnsForMuzykuj; minor changes

--- midi2png.php
+++ midi2png.php
@@ -1,15 +1,7 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(-1); 
-
-require_once('vendor/autoload.php');
-use Motniemtin\Midi\Midi;
-
 /*
 
-midi2png 2020 rewrite
+midi2png 2021 rewrite
 
 author: kamilbaranski (http://kamilbaranski.com/)
 license: freeware
@@ -19,7 +11,8 @@ let's write it once more with Midi class
 
 */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -30,17 +23,79 @@ let's write it once more with Midi class
 
 <body>
 	<pre>
-	
 <?php
 
+
+require_once('vendor/autoload.php');
+use Motniemtin\Midi\Midi;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$file = 'margaret_ifiaintgotyou.mid';
+$file = isset($_GET['file']) ? $_GET['file'] : 'margaret_ifiaintgotyou.mid';
+$fps = isset($_GET['fps']) ? $_GET['fps'] : 30;
 
 if (isset($file)) {
+	extractNoteOnsForMuzykuj($file, $fps);
+};
 
+
+/*
+ * prints "$frame $noteName [$velocity]\n" for each [Note On] on 1st track.
+ * written for muzykuj.com
+ */
+function extractNoteOnsForMuzykuj($file, $fps = 30) {
 	$midi = new Midi();
 	$midi->importMid($file);
+	$settings = getSettingsFromMidi($midi);
+	$settings['fps'] = $fps;
+	$noteList = array_map(
+		function ($noteName) {
+			return strtr($noteName, array('s' => '#'));
+		},
+		$midi->getNoteList()
+	);  																								// the original array is Cs4, but we like C#4
+
+	foreach ($midi->tracks[1] as $fullMessage) {														// we're interested only in first track
+		if (preg_match('/([0-9]+) (On) ch=([0-9]+) n=([0-9]+) v=([0-9]+)/', $fullMessage, $array)) {	// we're interested in NoteOn only
+			list($fullMessage, $timestamp, $message, $channel, $noteNumber, $velocity) = $array;
+			if ($velocity !== '0') {																	// some systems use [NoteOn with velocity=0] as [NoteOff].
+				echo getFrameFromTimestamp($timestamp, $settings) . " $noteList[$noteNumber] [$velocity]\n";
+			}
+		}
+	}
+};
+
+function getSettingsFromMidi($midi) {
+	$settings['tempo'] = $midi->tempo;
+	$settings['tempoBpm'] = 60000000 / $settings['tempo'];
+	$settings['timebase'] = $midi->timebase;
+	// echo 'tempoBpm = ' . $tempoBpm . "\n";
+	// echo 'timebase = ' . $timebase . "\n";		// often 480
+	return $settings;
+}
+
+function getFrameFromTimestamp($timestamp, $settings) {
+	$seconds = ($timestamp / $settings['timebase']) * ($settings['tempo'] / 1000000);
+	// $timestamp=$timestamp.' = '.number_format($seconds, 16);
+	$frame = round($seconds * $settings['fps']);
+	return $frame;
+};
+
+
+
+
+
+
+// *********************************
+// below is a draft; rewrite it!
+// *********************************
+// 
+function midi2png($file) {
+	$midi = new Midi();
+	$midi->importMid($file);
+	$noteList = $midi->getNoteList();
 
 	$tt = 0;		// 0: absolute times; 1: delta times
 
@@ -52,7 +107,15 @@ if (isset($file)) {
 		$message = $words[1];
 		if (($message === 'On') || ($message === 'Off') || (($message === 'Par') && ($words[3] === 'c=64'))) {		// we need only note on, note off and sustain.
 			list($timestamp, $message, $channel, $n, $v) = explode(' ', $fullMessage);
-			echo $fullMessage . "\n";
+			$regexp = '/([0-9]+) ([A-Za-z]+) ch=([0-9]+) [c|n]=([0-9]+) v=([0-9]+)/';
+			preg_match($regexp, $fullMessage, $array);
+
+			list($fullMessage, $timestamp, $message, $channel, $n, $v) = $array;
+			if (($message === 'On') and ($v !== '0')) {			// because some systems use [NoteOn with velocity=0] as [NoteOff].
+				$noteName = $noteList[$n];
+				echo $timestamp . ' ' . $noteName . ' [' . $v . ']' . "\n";
+			}
+			//echo $fullMessage . "\n";
 		}
 	}
 
@@ -60,8 +123,7 @@ if (isset($file)) {
 };
 
 ?>
-
-	</pre>
+</pre>
 </body>
 
 </html>
